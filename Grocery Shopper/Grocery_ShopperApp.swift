8e1c7192750475335d8edeb75c098e29834c51ff
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-01 18:30:45 +0530

    Grocery Shopper App: SwiftUI, Realm Database 04/06: Realm Shopping Store

--- Grocery Shopper/Grocery_ShopperApp.swift
+++ Grocery Shopper/Grocery_ShopperApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Grocery_ShopperApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(ShoppingStore(realm: RealPersistent.initializer()))
         }
     }
 }
