2826a2f356b82ab30062a2911e3ab794e36c98e6
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-01 16:56:24 +0530

    Initial Commit

---
+++ Grocery Shopper/Grocery_ShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ShopperApp.swift
+//  Grocery Shopper
+//
+//  Created by Mahesh Prasad on 01/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
